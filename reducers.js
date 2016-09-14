0246e16702a5b7388a5b19b948393dc0c55a50b3
Chris Chao <shubo@bridgewell.com>
2016-09-14 09:28:15 +0800

    Add reducers

---
+++ reducers.js
@@ -0,0 +1,46 @@
+import { combineReducers } from 'redux'
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return {
+        ...state,
+        visibilityFilter: action.filter
+      }
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      }
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return {
+              ...todo,
+              completed: !todo.completed
+            }
+          }
+
+          return todo
+        })
+      }
+    default:
+      return state
+  }
+}
+
+export default todoApp
