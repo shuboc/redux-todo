5b0241875dedb71ede600e29d54b4849ed908643
Chris Chao <shubo@bridgewell.com>
2016-09-14 15:16:06 +0800

    Add eslint config with jsx support

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": [
+        "standard",
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "plugins": [
+        "standard",
+        "promise",
+        "react"
+    ]
+};
