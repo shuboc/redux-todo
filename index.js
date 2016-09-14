8f7b8061a4c934080b326186d7e262d3786ef9cb
Chris Chao <shubo@bridgewell.com>
2016-09-14 11:35:05 +0800

    Test store functionalities

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import { createStore } from 'redux'
 import todoApp from './reducers'
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions'
 
 let store = createStore(todoApp)
 console.log(store.getState())
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(addTodo('Learn actions'))
+store.dispatch(addTodo('Learn reducers'))
+store.dispatch(addTodo('Learn store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+unsubscribe()
